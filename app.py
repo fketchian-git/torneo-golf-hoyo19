294d6f79717fc1f13af1ef4e0f819ba8275b6083
fketchian-git <fketchian@gmail.com>
2026-02-10 19:22:27 -0300

    Change SHEET_ID to a direct Google Sheets link
    Updated SHEET_ID to use a direct Google Sheets URL.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # --- CONFIGURACIÓN DE LA HOJA DE GOOGLE ---
 # Reemplaza con tu ID real de Google Sheets
-SHEET_ID = "TU_ID_DE_GOOGLE_SHEET"
+SHEET_ID = "https://docs.google.com/spreadsheets/d/1y3UgOy3gjJSJYM1GBmZYeATYhUZaz9Xv690CUX-m8Xw/edit?usp=sharing"
 SHEET_NAME = "scores"
 URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
 
